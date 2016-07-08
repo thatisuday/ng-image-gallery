8d044bcc682819e5f38121f47988f05847efd605
Uday Hiwarale <uhiwarale@gmail.com>
2016-07-08 17:14:44 +0530

    v0.0.1 commit

--- dist/main.js
+++ dist/main.js
@@ -10,8 +10,8 @@
 	};
 
 	angular
-	.module('thatisuday.ng-image-gallery', [])
-	.directive('ngImageGallery', ['$timeout', '$document', function($timeout, $document){
+	.module('thatisuday.ng-image-gallery', ['ngAnimate'])
+	.directive('ngImageGallery', ['$timeout', '$document', '$q', function($timeout, $document, $q){
 		return {
 			replace : true,
 			transclude : false,
@@ -19,38 +19,126 @@
 			scope : {
 				images : '=',
 				methods : '=?',
+				thumbnails : '@?',
 				onOpen : '&?',
 				onClose : '&?'
 			},
-			template : 	'<div class="ng-image-gallery hidden" ng-cloak>' +
-							'<div class="ng-image-gallery-backdrop"></div>'+
-							'<div class="ng-image-gallery-content">'+
-								'<div class="close" ng-click="methods.close();"></div>'+
-								'<div class="prev" ng-click="methods.prev();"></div>'+
-								'<div class="next" ng-click="methods.next();"></div>'+
-								'<div class="galleria">'+
-									'<div class="galleria-images">'+
-										'<img ng-repeat="image in images" ng-show="activeImg == image" ng-src="{{image.url}}" />'+
+			template : 	'<div class="ng-image-gallery">'+
+							'<div ng-if="thumbnails" class="ng-image-gallery-thumbnails">'+
+								'<div class="thumb" ng-repeat="image in images" ng-click="openFromThumb(image);" style="background-image:url({{image.thumbUrl || image.url}});"></div>'+
+							'</div>'+
+							'<div class="ng-image-gallery-modal" ng-show="opened" ng-cloak>' +
+								'<div class="ng-image-gallery-backdrop"></div>'+
+								'<div class="ng-image-gallery-content" ng-show="!imgLoading">'+
+									'<div class="close" ng-click="methods.close();"></div>'+
+									'<div class="prev" ng-click="methods.prev();"></div>'+
+									'<div class="next" ng-click="methods.next();"></div>'+
+									'<div class="galleria">'+
+										'<div class="galleria-images">'+
+											'<img ng-repeat="image in images" ng-if="activeImg == image" ng-src="{{image.url}}" />'+
+										'</div>'+
+										'<div class="galleria-bubbles">'+
+											'<span ng-click="setActiveImg(image);" ng-repeat="image in images" ng-class="{active : (activeImg == image)}"></span>'+
+										'</div>'+
 									'</div>'+
-									'<div class="galleria-thumbs"></div>'+
+								'</div>'+
+								'<div class="ng-image-gallery-loader" ng-show="imgLoading">'+
+									'<div class="spinner">'+
+										'<div class="rect1"></div>'+
+										'<div class="rect2"></div>'+
+										'<div class="rect3"></div>'+
+										'<div class="rect4"></div>'+
+										'<div class="rect5"></div>'+
+									'</div>'+
 								'</div>'+
 							'</div>'+
 						'</div>',
 			link : function(scope, elem, attr){
 				
+				/*
+				 *	Operational functions
+				**/
+
+				// Show gallery loader
+				scope.showLoader = function(){
+					scope.imgLoading = true;
+				}
+
+				// Hide gallery loader
+				scope.hideLoader = function(){
+					scope.imgLoading = false;
+				}
+
+				// Image load complete promise
+				scope.loadImg = function(imgObj){
+					var deferred =  $q.defer();
+
+					// Show loder
+					if(!imgObj.cached) scope.showLoader();
+
+					// Process image
+					var img = new Image();
+					img.src = imgObj.url;
+					img.onload = function(){
+						// Hide loder
+						if(!imgObj.cached) scope.hideLoader();
+
+						// Cache image
+						if(!imgObj.cached) imgObj.cached = true;
+
+						return deferred.resolve(imgObj);
+					}
+
+					return deferred.promise;
+				}
+
+				scope.setActiveImg = function(imgObj){
+					// Load image
+					scope.loadImg(imgObj).then(function(imgObj){
+						scope.activeImg = imgObj;
+					});
+				}
+
+				// Open gallery from thumbnail
+				scope.openFromThumb = function(imgObj){
+					scope.opened = true;
+					scope.setActiveImg(imgObj);
+				}
+
+				/***************************************************/
+				
+				/*
+				 *	Gallery settings
+				**/
+
 				// Modify scope models
 				scope.images = (scope.images) ? scope.images : [];
 				scope.methods = (scope.methods) ? scope.methods : {};
 				scope.onOpen = (scope.onOpen) ? scope.onOpen : angular.noop;
 				scope.onClose = (scope.onClose) ? scope.onClose : angular.noop;
 
-				// Gallery states
-				scope.activeImageIndex = 0;
-				scope.$watch('activeImageIndex', function(newIndex){
-					scope.activeImg = scope.images[newIndex];
+				// Watches
+				var imagesFirstWatch = true;
+				var activeImageIndexFirstWatch = true;
+
+				scope.$watch('images', function(){
+					if(imagesFirstWatch){
+						imagesFirstWatch = false;
+					}
+					else if(scope.images.length) scope.setActiveImg(
+						scope.images[scope.activeImageIndex]
+					);
+				});
+
+				scope.$watch('activeImageIndex', function(newImgIndex){
+					if(activeImageIndexFirstWatch){
+						activeImageIndexFirstWatch = false;
+					}
+					else if(scope.images.length) scope.setActiveImg(
+						scope.images[newImgIndex]
+					);
 				})
 				
-
 				/***************************************************/
 
 				/*
@@ -59,18 +147,25 @@
 
 				// Open modal
 				scope.methods.open = function(){
-					elem.removeClass('hidden fadeOut').addClass('fadeIn');
-					scope.onOpen(); // call open event
+					scope.activeImageIndex = 0; // Start from first image
+					scope.opened = true; // Model opened
+
+					 // call open event after transition
+					$timeout(function(){
+						scope.onOpen();
+					}, 300);
 				}
 
 
 				// Close modal
 				scope.methods.close = function(){
-					elem.removeClass('visible fadeIn').addClass('fadeOut');
+					scope.opened = false; // Model closed
+
+					// call close event after transition
 					$timeout(function(){
-						elem.addClass('hidden');
-						scope.onClose(); // call close event
-					},350);
+						scope.onClose();
+						scope.activeImageIndex = 0; // Reset index
+					}, 300);
 				}
 
 				// Change image to next
@@ -96,10 +191,10 @@
 				/***************************************************/
 
 				/*
-				 *	Interactions
+				 *	User interactions
 				**/
 				$document.bind('keyup', function(event){
-					if(event.which == keys.right){
+					if(event.which == keys.right || event.which == keys.enter){
 						$timeout(function(){
 							scope.methods.next();
 						});
